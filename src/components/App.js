56b7e5524a497c4af5181f7d80250b534c771433
AndreaAguado <andrea95am@gmail.com>
2021-09-24 09:13:25 +0200

    add html structure

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,46 @@ import '../styles/App.css';
 function App() {
   return (
     <div>
-      <h1>Hola Mundo!</h1>
+      <header>
+        <h1>Mis Clubs</h1>
+        <form action="">
+          <label htmlFor="schedule">Mostrar</label>
+          <select name="schedule" id="schedule">
+            <option value="everyday">Todos</option>
+            <option value="openOnWeekdays">Los que abren entre semana</option>
+            <option value="openOnWeekend">Los que abren en fin de semana</option>
+          </select>
+        </form>
+      </header>
+      <main>
+        <section>
+          <div>
+            <h2>Club</h2>
+            <p>Abierto entre semana:</p>
+            <p>Abierto el fin de semana:</p>
+          </div>
+        </section>
+        <section>
+          <h2>Añadir un nuevo club</h2>
+          <form className="addClub" action="">
+            <label htmlFor="name">Nombre del club</label>
+            <input id="name" type="text" />
+            <div className="checkboxes">
+              <label htmlFor="openOnWeekdays">
+              ¿Abre entre semana?
+                <input className="input"  type="checkbox" name="openOnWeekdays" id="openOnWeekdays" />
+              </label>
+              <label htmlFor="openOnWeekend">
+              ¿Abre los fines de semana?
+                <input className="input" type="checkbox" name="openOnWeekend" id="openOnWeekend" />
+              </label>             
+            </div>
+            <input className="submit" type="submit" value="Añadir un nuevo club" />
+          </form>
+        </section>
+      </main>
+
+
     </div>
   );
 }
